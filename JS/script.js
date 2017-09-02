6ea68cc33af66e95b5c9164b980a9aef171a6afb
trangtmtran <ttminhtrang09@gmail.com>
2017-09-02 14:35:46 +0300

    stop animation iteration

--- JS/script.js
+++ JS/script.js
@@ -11,23 +11,20 @@ $(document).ready(function() {
     } else {
         $('.nav-bg').removeClass('nav-show');
      	}
-      // if (y > $portfolioOffset) {
-      //  // do something
-      // } else {
-      //   // do something
-      // }
-      if (y > $skillsOffset) {
-          $(".skill").addClass("turn-right");
-          showSkill();
-          showPercent();
-        } else {
-          $(".skill").removeClass("turn-right");
-        }
-      if (y > $contactOffset - 200) {
-        $("#contact ul a").addClass("come-in");
+  
+    if (y > $skillsOffset -200) {
+        $(".skill").addClass("turn-right");
+        showSkill();
+        showPercent();
       } else {
-        $("#contact ul a").removeClass("come-in");
+        $(".skill").removeClass("turn-right");
       }
+
+    if (y > $contactOffset - 300) {
+      $("#contact ul a").addClass("come-in");
+    } else {
+      $("#contact ul a").removeClass("come-in");
+    }
   });
 
   //burger navigation
@@ -41,38 +38,20 @@ $(document).ready(function() {
   //   $(".overlay").toggleClass("drop");
   //   $("body").toggleClass("locked");
   // });
-  //project button click event handler
-  // var Projects = ["project-intro", "project-weather", "project-tictactoe", 
-  // "project-simon", "project-calculator", "project-twitch", 
-  // "project-random-quote", "project-pomodoro",
-  // "project-markdown", "project-leaderboard"];
-  // Projects.forEach(function(project) {
-  //   if (project !== Projects[0]) {
-  //     $('#' + project).hide(); //dont hide project-intro
-  //   }
-  //   //project names return id of that project's button
-  //   //using slice method
-  //   //ie. project-weather becomes weather
-  //   $('#'+project.slice(8)).click(function(e) {
-  //     $("#" + project).fadeIn().addClass("slide-in"); //this project will show up
-  //     //filter ones which are not this project
-  //     var filtered = Projects.filter(function(proj) {
-  //       return proj !== project;
-  //     });
-  //     //and hide them
-  //     filtered.forEach(function(item) {
-  //         $('#' + item).hide().removeClass("slide-in");
-  //     })
-  //   });
-  // });
 
   //Skill set
-  function showSkill() {
-    $('.skill').each(function(){
-      $(this).find('.skillbar-bar').animate({
+
+  var animate = true;
+
+  function showSkill() { //fired just once
+    if(animate) {
+      $('.skill').each(function(){
+        $(this).find('.skillbar-bar').animate({
         width:$(this).attr('data-percent')
-        },2500);    
-  })
+        },2500); 
+        animate = false;
+      })     
+  }
 }
 
   function showPercent() {
